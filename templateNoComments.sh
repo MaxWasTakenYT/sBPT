592824cfa2a40b023c97fc3b214eb2d2271082a0
jstmax! <87650746+MaxWasTakenYT@users.noreply.github.com>
2024-10-10 17:11:49 +0000

    Create templateNoComments.sh

---
+++ templateNoComments.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+usage() {
+    echo -e "usage: $0 \n[--test | -t > Description for the argument]\n [--help | -h > e.g. Outputs usage and exits.]\n"
+}
+
+if [[ "$#" -eq 0]]; then
+    echo -e "no arguments were given"
+fi
+
+while [[ "$#" -gt 0]]; do
+    case "$1" in
+        --test|-t)
+            echo -e "test parsed"
+            shift
+            ;;
+        --help|-h)
+          usage
+          shift
+          ;;
+        *)
+            echo -e "unknown option: $1"
+            usage
+            ;;
+        esac
+done
